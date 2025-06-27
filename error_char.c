33f267f6ebed90a46ebf80c97584018f3ce29b58
BorisKin1957 <6063354@gmail.com>
2025-06-27 10:44:53 +0700

    Исправил косяк VSCode

---
+++ error_char.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    unsigned char c;
+    unsigned int a, b;
+    scanf("%u %u", &a, &b);
+
+    if (a <= 0xff) c = (unsigned char)a;
+        else {
+            printf("Ошибка: a > 255\n");
+            return 0;
+            }
+    if (b > 0xff){
+            printf("Ошибка: b > 255\n");
+            return 0;
+            }
+
+    printf("a = %u, b = %u\n", c, b);
+    return 0;
+
+}
+
+/*
+Всегда выводит ПЕРВУЮ переменную равной нулю! Т.е. a = 0;
+Как только char меняю на int всё работает правильно.
+Ввел новую переменную c, которая принимает значение a, 
+и далее исполняеет роль переменной char.
+
+*/
