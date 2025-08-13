6047500b7eeefe1f5477540c998fa94ccd5d5617
BorisKin1957 <6063354@gmail.com>
2025-08-13 17:56:11 +0700

    6.4 Указатели как параметры. Передача массивов в функции

--- 6_4_05.c
+++ 6_4_05.c
@@ -1,34 +1,82 @@
+/* Необходимо объявить функцию с именем transpose, которой передается двумерный
+массив размером SIZE x SIZE из чисел типа short. Функция transpose должна строки
+заменять на соответствующие столбцы (транспонирование) переданного массива. В
+функции main вызовите функцию transpose для массива matrix. Выведите полученный
+транспонированный массив matrix в консоль в виде таблицы целых чисел SIZE x SIZE
+элементов.
+
+(Обратите внимание, что между числами в строках должен быть один пробел, а в
+конце строк - ни одного.)
+
+Тесты: https://github.com/selfedu-rus/c-tests/tree/main/6.4.6
+
+Sample Input:
+
+1 2 3 4
+5 6 7 8
+9 8 7 6
+5 4 3 2
+
+Sample Output:
+
+1 5 9 5
+2 6 8 4
+3 7 7 3
+4 8 6 2
+
+
+*/
+
 #include <stdio.h>
 
-#define SIZE    4 // размер матрицы
+#define SIZE 4 // размер матрицы
 
-int transpose(short matrix[SIZE][SIZE]);
+// Объявление функции транспонирования
+void transpose(short matrix[SIZE][SIZE]);
 
-
-
-int main(void)
-{
-    short matrix[SIZE][SIZE] = {0}; // инициализация матрицы нулями
-    short* ptr = &matrix[0][0]; // указатель на первый элемент матрицы
+int main(void) {
+    short matrix[SIZE][SIZE]; // инициализация матрицы
+    short *ptr = &matrix[0][0]; // указатель на первый элемент
     int count = 0;
-    while(count < SIZE * SIZE && scanf("%hd", ptr) == 1) { // считываем значения
-        ptr++; // перемещаем указатель на следующий элемент
-        count++; // увеличиваем счетчик элементов
+
+    // Чтение данных из stdin
+    while (count < SIZE * SIZE && scanf("%hd", ptr) == 1) {
+        ptr++; // перемещаем указатель
+        count++; // увеличиваем счетчик
     }
 
-    // здесь продолжайте программу
+    // Вызов функции транспонирования
+    transpose(matrix);
 
-    transpose(matrix);  // Вызов функции транспонирования
+    // Вывод результата в виде таблицы
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (j != SIZE - 1)
+                printf("%hd ", matrix[i][j]); // вывод с пробелом
+            else
+                printf("%hd", matrix[i][j]); // последний элемент без пробела
+        }
+        printf("\n"); // переход на новую строку
+    }
 
     return 0;
 }
 
-int transpose(short matrix[SIZE][SIZE]){
-    for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; j++){
-            printf("%hd ", matrix[j][i]);// Транспонирование: строки ↔ столбцы
+// Функция транспонирования матрицы
+void transpose(short matrix[SIZE][SIZE]) {
+    short result[SIZE][SIZE]; // временная матрица для хранения результата
+
+    // Транспонирование матрицы
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = matrix[j][i]; // перенос значений
         }
-        printf("\n");
     }
-    return 0;
+
+    // Копирование результата обратно в исходную матрицу
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            matrix[i][j] = result[i][j];
+        }
+    }
 }
